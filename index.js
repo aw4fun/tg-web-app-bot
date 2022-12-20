30f578a94bd15dfacda0f54efdffe73a45d004d4
Belokon Andrey <belokonandrey@MacBook-Pro-Belokon.local>
2022-12-20 20:36:27 +0300

    added queryId check

--- index.js
+++ index.js
@@ -93,16 +93,16 @@ bot.on('message', async (msg) => {
 
 
 app.post('/web-data', async (req, res) => {
-    // const {queryId, products = [], totalPrice} = req.body;
+    const {queryId, products = [], totalPrice} = req.body;
     try {
-        // await bot.answerWebAppQuery(queryId, {
-        //     type: 'article',
-        //     id: queryId,
-        //     title: 'Успешная покупка',
-        //     input_message_content: {
-        //         message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
-        //     }
-        // })
+        await bot.answerWebAppQuery(queryId, {
+            type: 'article',
+            id: queryId,
+            title: 'Успешная покупка',
+            // input_message_content: {
+            //     message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
+            // }
+        })
         return res.status(200).json(req.body);
     } catch (e) {
         return res.status(500).json({})
