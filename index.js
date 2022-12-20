ae9a9a432f103cdc5ca4044a0bf8ab8730c799d0
Belokon Andrey <belokonandrey@MacBook-Pro-Belokon.local>
2022-12-20 19:45:36 +0300

    added url fix

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ bot.on('message', async (msg) => {
 });
 
 
-app.post('web-data', async (req, res) => {
+app.post('/web-data', async (req, res) => {
     const { queryId, bet_data } = req.body;
 
     console.log('triggered!');
