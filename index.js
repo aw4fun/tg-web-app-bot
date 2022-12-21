2dd7a684ffeebb9d809b27881a65c5247b990669
Belokon Andrey <belokonandrey@MacBook-Pro-Belokon.local>
2022-12-21 13:23:34 +0300

    added body error

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ app.post('/web-products', async (req, res) => {
         })
         return res.status(200).json({});
     } catch (e) {
-        return res.status(500).json(e.message)
+        return res.status(500).json({error: e.message})
     }
 })
 
