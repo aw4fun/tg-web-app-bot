314b5fa2ab1d8848d1d21bb33847713cbc3f7275
Belokon Andrey <belokonandrey@MacBook-Pro-Belokon.local>
2022-12-21 10:43:40 +0300

    moved data length

--- index.js
+++ index.js
@@ -99,9 +99,9 @@ app.post('/web-data', async (req, res) => {
             type: 'article',
             id: queryId,
             title: 'Успешная покупка',
-            message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${products.length}`,
+            message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму`,
             input_message_content: {
-                message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму23`
+                message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму23  ${products.length}`
             }
         })
         return res.status(200).json(req.body);
