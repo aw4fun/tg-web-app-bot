30ecdd9d57d5312e38974ed7fd5e93c6bd9054b4
Belokon Andrey <belokonandrey@MacBook-Pro-Belokon.local>
2022-12-21 11:55:13 +0300

    adde endpoint web-products

--- index.js
+++ index.js
@@ -110,6 +110,24 @@ app.post('/web-data', async (req, res) => {
     }
 })
 
+app.post('/web-products', async (req, res) => {
+    const {queryId, products = [], totalPrice} = req.body;
+    try {
+        await bot.answerWebAppQuery(queryId, {
+            type: 'article',
+            id: queryId,
+            title: 'Успешная покупка',
+            message_text: `Required field`,
+            input_message_content: {
+                message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
+            }
+        })
+        return res.status(200).json({});
+    } catch (e) {
+        return res.status(500).json({})
+    }
+})
+
 const PORT = 8000;
 
 app.listen(PORT, () => console.log('server started on PORT ' + PORT))
